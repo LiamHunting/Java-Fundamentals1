4d56b4394e18a0e723486abd0deebbb7e01217fb
Liam Hunting <hunting.liam@gmail.com>
2026-01-22 15:16:02 -0700

    Some more code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Decalring and initializing integer variables
         int myAge = 25;
         int anotherAge = myAge;
         System.out.println(myAge);
         System.out.println(anotherAge);
+        // Modifying anotherAge
+        anotherAge = 30;
+        System.out.println(myAge);
+        System.out.println(anotherAge);
+
+        
     }
 }
