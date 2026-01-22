7d7c713619954deefe6d78aaed12022ff4cddc2c
Liam Hunting <hunting.liam@gmail.com>
2026-01-22 09:07:30 -0700

    First code push

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int myAge = 25;
+        int anotherAge = myAge;
+        System.out.println(myAge);
+        System.out.println(anotherAge);
+    }
+}
